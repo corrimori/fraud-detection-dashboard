ec44899fb4963b0425731bfa84203bf963145b44
Corrine Morita <corrine@promptt.com>
2018-07-25 13:10:23 -0700

    remove boilerplate react items

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Button} from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
+  state = {
+    messages: [
+      {
+        "event_id": 1,
+        "description": "You can't input the protocol without calculating the mobile RSS protocol!",
+        "facebook": false,
+        "starred": true,
+        "labels": ["high", "medium"]
+      },
+      {
+        "id": 2,
+        "subject": "connecting the system won't do anything, we need to input the mobile AI panel!",
+        "read": false,
+        "starred": false,
+        "selected": true,
+        "labels": []
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button>Click Here</Button>
       </div>
     );
   }
